1e58e5d2e882831745913ad6c603625f0a5d6600
Ayman Nedjmeddine <theycallmethedr@gmail.com>
2021-12-15 10:29:42 +0100

    Reset

--- src/app/models/patient.ts
+++
@@ -1,9 +0,0 @@
-export interface Patient {
-    displayName: string;
-    phoneNumber: string;
-    created_by: string;
-    created_at: Date;
-    lastUpdate?: Date | 'Not updated';
-    order?: number;
-    rdvID?: string;
-}
